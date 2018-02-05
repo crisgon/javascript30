c1f824e79e60fa62ce5caf3bf0af1ad969290623
crisgon <kristiano.gon@gmail.com>
2018-02-04 22:09:45 -0300

    Day 6 - Ajax

--- 6-Ajax/ajax.js
+++ 6-Ajax/ajax.js
@@ -32,7 +32,40 @@ const cities = [];
 // Forma mais enxuta, sem passar o metodo get
 fetch(URL_TO_FETCH)
 .then((response) => response.json())
-.then((data) => cities.push(data))
+.then((data) => cities.push(...data))
 .catch((err) => console.log('Falha! Erro:' + err));
 
-console.log(cities);
+
+
+function findCities (wordToMatch, cities) {
+  return cities.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayCities (){
+  const matchArray = findCities(this.value, cities)
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+    stateName = place.city.replace(regex, `<span class="hl">${this.value}</span>`)
+    return `<li>
+              <span class="name">${cityName}, ${stateName}</span>
+              <span class="population">${numberWithCommas(place.population)}</span>
+            </li>`;
+  }).join('');
+  suggestions.innerHTML = html;  
+}
+
+const inputSearch = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+inputSearch.addEventListener('change', displayCities);
+inputSearch.addEventListener('keyup', displayCities);
+
+
